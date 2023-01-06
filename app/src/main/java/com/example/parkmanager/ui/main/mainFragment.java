0a6f247ffaf2a581ae56fd840ff8f01feaf87aa3
Tim Federolf <Tim.Federolf@gmx.com>
2023-01-06 10:38:17 +0100

    Added OkHttp-Integration. Example is in Main Fragment

--- app/src/main/java/com/example/parkmanager/ui/main/mainFragment.java
+++ app/src/main/java/com/example/parkmanager/ui/main/mainFragment.java
@@ -14,7 +14,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.parkmanager.MainActivity;
 import com.example.parkmanager.databinding.FragmentMainBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 
 public class mainFragment extends Fragment {
@@ -24,9 +41,46 @@ public class mainFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+
         binding.btnFragmentA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                /*
+                OkHttpClient client = new OkHttpClient();
+
+                String url = "http://10.0.2.2:5000/all/parkingplace";
+
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                        if(response.isSuccessful()){
+                            String myResponse = response.body().string();
+
+
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    binding.jsonId.setText(myResponse);
+                                }
+                            });
+
+
+                        }
+                    }
+                });
+
+                 */
+
+
                 //Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_fragmentA);
                 //NavHostFragment.findNavController(mainFragment.this)
                         //.navigate(R.id.action_mainFragment_to_fragmentA);
